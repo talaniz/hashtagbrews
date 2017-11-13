89e6f1a879c48806a75a4ffbdddb0720e768d255
sandwichheat <antonio.alaniz@gmail.com>
2017-11-13 00:15:18 -0800

    Added pre-auth checks in unit tests.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -15,6 +15,27 @@ class TestLoginPage(TestCase):
         self.user = User.objects.create_user(username='testuser', email="antonio.alaniz@gmail.com",
                                              password='testpassword')
 
+    def pre_login_check(self, model):
+        """Check that modal links aren't showing in each list view."""
+
+        # FIXME: Grains model needs to be updated to use plural
+        # otherwise it has to check for which model and provide the match string
+        if model == 'grains':
+            add_match = '#deletegrain'
+            update_match = '#updategrain'
+            delete_match = '#addgrain'
+        else:
+            add_match = '#delete{}'
+            update_match = '#update{}'
+            delete_match = '#add{}'
+
+        response = self.client.get('/beerdb/{}/'.format(model))
+        self.assertIn('Login', response.content.decode())
+        self.assertNotIn('testuser', response.content.decode())
+        self.assertNotIn(delete_match.format(model), response.content.decode())
+        self.assertNotIn(update_match.format(model), response.content.decode())
+        self.assertNotIn(add_match.format(model), response.content.decode())
+
     def test_login_returns_correct_template(self):
         """Login view should return login.html.
                 :return: pass or fail
@@ -29,9 +50,9 @@ class TestLoginPage(TestCase):
 
     def test_login_page_auths_on_POST(self):
         """`login` view should auth & redir to the homebrew db page."""
-        response = self.client.get('/beerdb/hops/')
-        self.assertIn('Login', response.content.decode())
-        self.assertNotIn('testuser', response.content.decode())
+        self.pre_login_check('hops')
+        self.pre_login_check('grains')
+        self.pre_login_check('yeasts')
 
         response = self.client.login(username='testuser', password='testpassword')
         self.assertEqual(response, True)
@@ -49,11 +70,12 @@ class TestLoginPage(TestCase):
         self.assertIn('Homebrew Materials Database', response.content.decode())
         self.assertIn('testuser', response.content.decode())
 
+        response = self.client.get('/beerdb/hops/')
+        self.assertIn('#addhops', response.content.decode())
+
     def test_login_page_redirects_to_next(self):
         """`login` view should auth & redir to the next url if specified."""
-        response = self.client.get('/beerdb/hops/')
-        self.assertIn('Login', response.content.decode())
-        self.assertNotIn('testuser', response.content.decode())
+        self.pre_login_check('hops')
 
         response = self.client.post(
             '/accounts/login/',
